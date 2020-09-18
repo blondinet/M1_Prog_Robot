6f4b54b1731c143b390a345106bc48e9dcf02f5f
blondinet <xavier.jacog--guizon@orange.fr>
2020-09-18 06:47:32 +0200

    ajout du td1

---
+++ TD1_Jon/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		/*Ex1 exo1 = new Ex1();
+		exo1.sayHello();*/
+		
+		/*Ex2 exo2 = new Ex2();
+		exo2.driveForward(50, 5000); //(speed percentage, delay in milliseconds)*/
+		
+		Ex3 exo3 = new Ex3();
+		exo3.turnAround(50, 2000);
+	}
+	
+}
