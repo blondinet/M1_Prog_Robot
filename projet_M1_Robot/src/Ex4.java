b866af4de3d034bd0986118b5128444eafdf94fb
Lixye <nounoursdu88@live.fr>
2020-09-18 09:28:43 +0200

    suppression de TD1_JON la menace

--- projet_M1_Robot/src/Ex4.java
+++ projet_M1_Robot/src/Ex4.java
@@ -6,8 +6,7 @@ import objetPhysic.*;
 public class Ex4 {
 
 	Components comp;
-	UnregulatedMotor[] motors = {comp.motorB, comp.motorC};
-	int[] directions = {0,1}; //0=avant, 1=arrière
+
 	
 	public Ex4() {
 		comp = new Components();
@@ -15,15 +14,15 @@ public class Ex4 {
 	
 	public void danceAwayYourProblems() {
 		Button.waitForAnyPress();
-		System.out.println("Drive in a Square\nand Stop\n");
+		System.out.println("DANCE AWAY YOUR PROBLEMS!\n");
 		System.out.println("Press any key to start");
 		
 	}
 	
 	public void doStep(Components motors, int directions) { //dir = avant ou arrière
-		
+		int dir= (int)(Math.random()*100);
 		while (Button.getButtons()!=Button.ID_ENTER) {
-			int dir= (int)(Math.random()*100);
+			
 		}
 		
 	}
