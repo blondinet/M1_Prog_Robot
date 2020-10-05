aa92e8c88a607ad6c54f1939181ed91b84932626
Lixye <nounoursdu88@live.fr>
2020-10-05 15:19:37 +0200

    ajout classe main pour le projet twister

--- src/behaviors/DriveForward.java
+++ src/behaviors/DriveForward.java
@@ -1,5 +1,16 @@
 package behaviors;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.subsumption.Behavior;
 
-public class DriveForward {
-
+public class DriveForward implements Behavior{
+	
+	public boolean takeControl() { return true; }
+	public void suppress() {
+	Motor.B.stop(true);
+	Motor.C.stop(true);
+	}
+	public void action() {
+	Motor.B.forward();
+	Motor.C.forward();
+	}
 }
---
+++ src/main_Package/TwisterMain.java
@@ -0,0 +1,10 @@
+package main_Package;
+
+public class TwisterMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
