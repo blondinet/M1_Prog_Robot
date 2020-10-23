36ac2144315172f536f22c18269cb4da317fa433
Lixye <nounoursdu88@live.fr>
2020-10-23 09:36:18 +0200

    ajout comportement pour tester la fonction goTo du navigator

--- src/behaviors/goTo.java
+++ src/behaviors/goTo.java
@@ -1,24 +1,30 @@
 package behaviors;
 
 import lejos.robotics.subsumption.Behavior;
+import lejos.robotics.navigation.Navigator;
+import ressources_twister.Robot;
 
 public class goTo implements Behavior {
 
+	private Navigator nav;
+	
+	public goTo(Navigator n) {
+		this.nav = n;
+	}
 	@Override
 	public boolean takeControl() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-		
+		nav.goTo(200,200);
 	}
 
 	@Override
 	public void suppress() {
-		// TODO Auto-generated method stub
+		nav.stop();
 		
 	}
 
