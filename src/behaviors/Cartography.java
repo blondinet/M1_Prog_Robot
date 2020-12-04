2f6d082341c0c5b8885dcb43c2e2b07d36acbec5
Lixye <nounoursdu88@live.fr>
2020-12-04 16:11:53 +0100

    modif cartography behavior

--- src/behaviors/Cartography.java
+++ src/behaviors/Cartography.java
@@ -4,6 +4,7 @@ import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 import ressources_twister.Enregistreur;
 import ressources_twister.Robot;
 
@@ -23,40 +24,40 @@ public class Cartography implements Behavior {
 		LCD.drawString("Cartographie", 0, 0);
 		LCD.drawString("Chargement...", 0, 1);
 		int dist_case = 133;
-		int angle = 150;
 		Enregistreur.resetMemoireMap();
 		LCD.clear(1);
 		LCD.clear(2);
 		LCD.drawString("Place moi sur case", 0, 1); // case rouge dans le coin en bas à gauche
 		LCD.drawString("rouge & touche moi", 0, 2);
 		Button.waitForAnyPress();
-
+		
 		// début de la cartographie
+		this.robot.getPilot().setAngularSpeed(100);
+		this.robot.getPilot().setLinearSpeed(100);
 		for (int j=0; j<this.robot.getMapMemoire().lengthY(); j++) {
 			if ((j%2)==0) { // si on est sur une colonne paire = robot va dans un sens (aller)
 				for (int i=0; i<this.robot.getMapMemoire().lengthX(); i++) {
 					// attribution de la couleur de la case
 					///// COULEURS
-					///////this.getMapMemoire().getCase(i, j).setCouleur(this.comparerCouleur());
+					//this.robot.getMapMemoire().getCase(i, j).setCouleur(this.robot.comparerCouleur());
 					// comportements pour passer à la case ou ligne suivante
 					if(i < this.robot.getMapMemoire().lengthX()-1) {
 						// comportement classique
 						this.robot.getPilot().travel(dist_case);
-						//arbitrator_case_suivante.go();
 					} else {
 						// comportement pour la dernière case de la ligne
 						///// ROTATION
-						//////this.getLeftW().stop(true);
-						this.robot.getPilot().rotate(-angle);
-						//////this.getLeftW().forward();
-						//arbitrator_ligne_suivante.go();
+						this.robot.getPilot().travel(dist_case);
+						this.robot.getPilot().rotate(340);
+						
 					}
+					
 				}
 			} else { // si on est sur une colonne impaire = robot va dans l'autre sens (retour)
 				for (int i=this.robot.getMapMemoire().lengthX()-1; i>=0; i--) {
 					// attribution de la couleur de la case
 					///// COULEURS
-					//////this.getMapMemoire().getCase(i, j).setCouleur(this.comparerCouleur());
+					//this.robot.getMapMemoire().getCase(i, j).setCouleur(this.robot.comparerCouleur());
 					// comportements pour passer à la case ou ligne suivante
 					if(i > 0) {
 						// comportement classique
@@ -65,14 +66,14 @@ public class Cartography implements Behavior {
 					} else {
 						// comportement pour la dernière case de la ligne
 						///// ROTATION
-						/////this.getRightW().stop(true);
-						this.robot.getPilot().rotate(angle);
-						/////this.getRightW().forward();
-						//arbitrator_ligne_suivante.go();
+						this.robot.getPilot().travel(dist_case);
+						this.robot.getPilot().rotate(-340);
+					
 					}
 				}
 			}
 		}
+		
 		//arbitrator_ligne_suivante.go();
 		
 			
