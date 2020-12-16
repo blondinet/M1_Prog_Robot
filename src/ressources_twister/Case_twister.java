a500e37a1ccdf6dd9d349c9bd1c87f584f887ba8
WilliamTrdt <williamtrdt@gmail.com>
2020-12-16 11:42:31 +0100

    javadoc case & map

--- src/ressources_twister/Case_twister.java
+++ src/ressources_twister/Case_twister.java
@@ -2,17 +2,36 @@ package ressources_twister;
 
 import java.io.Serializable;
 
-public class Case_twister implements Serializable{
+/**
+ * Classe qui permet d'instancier une case.
+ * 
+ * @author Lucille Dumont & William Tardot
+ *
+ */
+public class Case_twister implements Serializable {
+	// Variable
 	private Color_twister couleur;
 	private boolean libre;
 	private int posX;
 	private int posY;
-	
+
+	// Comportement
+	/**
+	 * Constructeur d'une case indépendamment d'une carte (sans position x et y).
+	 * Par défaut une case est libre et sa couleur est nulle.
+	 */
 	public Case_twister() {
 		this.couleur = null;
 		this.libre = true;
 	}
-	
+
+	/**
+	 * Constructeur d'une case dans une carte (avec sa position x et y). Par défaut
+	 * une case est libre et sa couleur est nulle.
+	 * 
+	 * @param x position x de la case
+	 * @param y position y de la case
+	 */
 	public Case_twister(int x, int y) {
 		this.posX = x;
 		this.posY = y;
@@ -20,6 +39,7 @@ public class Case_twister implements Serializable{
 		this.libre = true;
 	}
 
+	// Getters & Setters
 	public Color_twister getCouleur() {
 		return couleur;
 	}
@@ -51,11 +71,15 @@ public class Case_twister implements Serializable{
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-	
+
+	// Méthodes
+	/**
+	 * Permet d'afficher une case sous forme de chaîne de caractères. Une case
+	 * s'affiche sous la forme suivante :
+	 * "Case(0,0), couleur : rouge"
+	 */
 	public String toString() {
-		   return "Case(" + this.posX +
-			  "," + this.posY +
-			  "), couleur : " + this.couleur;
-		}
-	
+		return "Case(" + this.posX + "," + this.posY + "), couleur : " + this.couleur;
+	}
+
 }
--- src/ressources_twister/Map_twister.java
+++ src/ressources_twister/Map_twister.java
@@ -2,32 +2,36 @@ package ressources_twister;
 
 import java.io.Serializable;
 
+/**
+ * Classe qui permet d'instancier une carte de jeu de Twister.
+ * 
+ * @author Lucille Dumont & William Tardot
+ *
+ */
 public class Map_twister implements Serializable {
+	// Variables
 	private Case_twister[][] map;
-	final static private int X = 5;
-	final static private int Y = 7;
+	final static private int X = 5; // Largeur du tableau
+	final static private int Y = 7; // Longueur du tableau
+	// Constructeur
+
 	/**
-	 * Constructeur de la classe
-	 * Une couleur est composé de 3 valeurs numériques allant de 0 à 255 (rouge, vert, bleu)
+	 * Constructeur de la classe carte. Créé un tableau à double entrée de 5 cases
+	 * sur 7
+	 * 
 	 */
 	public Map_twister() {
 		this.map = new Case_twister[X][Y];
-		/*
-		for(Case_twister[] l:this.map) {
-			for(Case_twister c:l) {
-				c = new Case_twister();
-			}
-		}*/
 		// Création d'un tableau de couleur de 5x7 cases
-		for (int y=0; y<Y; y++) {
-			for (int x=0; x<X; x++) {
-				Case_twister c = new Case_twister(x,y);
+		for (int y = 0; y < Y; y++) {
+			for (int x = 0; x < X; x++) {
+				Case_twister c = new Case_twister(x, y);
 				this.map[x][y] = c;
-				
 			}
 		}
 	}
 
+	// Getters & Setters
 	public Case_twister[][] getMap() {
 		return map;
 	}
@@ -35,36 +39,46 @@ public class Map_twister implements Serializable {
 	public void setMap(Case_twister[][] map) {
 		this.map = map;
 	}
-	
+
 	public Case_twister getCase(int x, int y) {
 		return map[x][y];
 	}
-	
+
 	public int lengthX() {
 		return X;
 	}
+
 	public int lengthY() {
 		return Y;
 	}
-	
+
+	// Méthodes
+	/**
+	 * Permet d'afficher la map sous forme de chaîne de caractères. Une map
+	 * s'affichera sous la forme suivante :
+	 * "|x|x|x|x|x|x|x|
+	 *  |x|x|x|x|x|x|x|
+	 *  |x|x|x|x|x|x|x|
+	 *  |x|x|x|x|x|x|x|
+	 *  |x|x|x|x|x|x|x|"
+	 */
 	public String toString() {
-		String result="";
-		
-		for (int x=0; x<X; x++) {
-			result+="|";
-			for (int y=0; y<Y; y++) {
-				if(this.map[x][y].getCouleur()!= null) {
-					result+=this.map[x][y].getCouleur().getName().charAt(0);
+		String result = "";
+
+		for (int x = 0; x < X; x++) {
+			result += "|";
+			for (int y = 0; y < Y; y++) {
+				if (this.map[x][y].getCouleur() != null) {
+					result += this.map[x][y].getCouleur().getName().charAt(0);
+				} else {
+					result += "/";
 				}
-				else {
-					result+="/";
-				}
-				result+="|";
-				
+				result += "|";
+
 			}
-			result+="\n";
+			result += "\n";
 		}
-		
+
 		return result;
 	}
 }
